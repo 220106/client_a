d9746edf4ad1e5d3941b4946af2b8e86c81ca75e
220106 <220106rq@yse-c.net>
2023-11-08 11:51:51 +0900

    new

---
+++ main.js
@@ -0,0 +1,18 @@
+// dotenvモジュールを利用するための設定
+import dotenv from 'dotenv';
+dotenv.config();
+
+// 環境変数を取得
+const name = process.env.NAME;
+const number = process.env.NUMBER;
+
+// message.jsモジュールをインポート
+import { create } from './message.js';
+
+// create 関数を使用してメッセージを生成
+const result = create(name, number);
+
+
+// fsモジュールを利用して、結果を外部ファイル「result.txt」に書き出す
+import fs from 'fs';
+fs.writeFileSync('result.txt', result);
---
+++ message.js
@@ -0,0 +1,3 @@
+export function create(name, number) {
+  return `${name}の学籍番号は${number}です。`;
+}
